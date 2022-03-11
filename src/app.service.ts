5b45961c9b2e6d50a37792a33a0ff663b37a00a3
Rodrigo Catalán López <ext_recatalanl@falabella.cl>
2022-03-11 11:09:46 -0300

    arr de user

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+const arr = [
+  'Pablo', 'Juan', 'Diego'
+]
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUser(): any {
+    return arr ;
   }
 }
